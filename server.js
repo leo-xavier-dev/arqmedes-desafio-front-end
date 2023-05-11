dbcffc6b570831e364650df27befcbf4172ee156
leo-xavier-dev <prof.leonardoxavier@gmail.com>
2023-05-11 14:44:24 -0300

    fix(package.json): Alteração para publicação

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running')
 })
 
